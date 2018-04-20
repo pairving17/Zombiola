d02701a734e4050fe23d06cd03e557cf0a1a8853
pairving17 <38569548+pairving17@users.noreply.github.com>
2018-04-20 14:42:34 -0400

    Add files via upload

--- Zombie_Simulation_v1.2.py
+++ Zombie_Simulation_v1.2.py
@@ -5,21 +5,18 @@ Jorge Alejandre
 Rodrigo Lapenne
 Curtis Lee
 
-4/18/18
+4/20/18
 v1.2
 ***
 """
 
 import random
-import numpy as np # can now be used as "np.- whatever function call"
-import matplotlib.pyplot as plt # can now be used as "plt.- whatever function call"
-
 island_Size = 50                    # island is a square
-peps = int(input("How many Peps: "))  # peps is Person Objs
-zams = int(input("How many Zams: "))  # zams is Zombie Objs
-sick = int(input("How many Sick: "))  # Sick is Infected Objs
+peps = int(input("How many Peps"))  # peps is Person Objs
+zams = int(input("How many Zams"))  # zams is Zombie Objs
+sick = int(input("How many Sick"))  # Sick is Infected Objs
 personList = []                     # to contain peps objs
-zombieList = []                     # to contain zams objs
+zambieList = []                     # to contain zams objs
 sickList = []                       # to contain zams objs
 
 
@@ -46,10 +43,6 @@ class Person :
     # returns indent
     def get_ID(self):
         return self.ID
-    
-    
-        
-        
     
     # the walk func that moves the X & Y forward, backward, sideways, nowhere
     def walk(self):
@@ -83,84 +76,59 @@ class Person :
             """condit 4 if pep at MAX island val"""
             self.y += random.randint(-1,0)
             
-# Function for printing peps
-for i in range(peps):
-    """loops thro person Objs in personList"""
-    personList.append(Person(i,0,0))
-    
-bFlag = True   #boolean flag
-time = 0       # time var 
-while bFlag:
-    for p in personList:
-        """loop 4 peps walking"""
-        p.walk()
-        
-    print("t = {}, walks complete".format(time))
-    time +=1
-    
-    if time == 100:
-        bFlag = False 
-print("")
-
-for p in personList:
-    print(p.__str__(), ", x =", p.get_x(), ", y =", p.get_y())
-
-for p2 in personList:
-    if peps[p2].get_x() == zams[p2].get_x() and peps[p2].get_y() == zams[p2].get_y():
-        sickList.append(__init__(self.__str__(),self.get_x(),self.get_y()))
-    
 class Zombie(Person):
     def __str__(self):
         """creates string repersentation of the obj"""
         return "Zam {}".format(self.ID)
      
-# Function for printing Zombies    
-for i in range(zams):
-    """loops thro Zombie Objs in zombieList"""
-    zombieList.append(Zombie(i,random.randint(0,50),random.randint(0,50)))
-    
-bFlag = True   # boolean flag
-time = 0       # time var 
-while bFlag:
-    for z in zombieList:
-        """loop 4 zams walking"""
-        z.walk()
-        
-    print("t = {}, walks complete".format(time))
-    time +=1
-    
-    if time == 100:
-        bFlag = False 
-print("")
-for z in zombieList:
-    print(z.__str__(), ", x =", z.get_x(), ", y =", z.get_y())
-
-           
 class Infected(Person):
     def __str__(self):
         """creates string repersentation of the obj"""
         return "Sick {}".format(self.ID)
     
-    
-# Funtion for printing Infected
+ID_giver=0
+# Function creates People objs
+for i in range(peps):
+    """loops thro person Objs in personList"""
+    personList.append(Person(ID_giver,random.randint(0,50),random.randint(0,50)))
+    ID_giver+=1
+# Function creates Zombies objs    
+for i in range(zams):
+    """loops thro Zombie Objs in zambieList"""
+    zambieList.append(Zombie(ID_giver,random.randint(0,50),random.randint(0,50)))
+    ID_giver+=1
+
+# Funtion creates Infected objs
 for i in range(sick):
     """loops thro person Objs in personList"""
-    sickList.append(Infected(i,0,0))
-    
-bFlag = True   #boolean flag
-time = 0       # time var 
+    sickList.append(Infected(ID_giver,random.randint(0,50),random.randint(0,50)))
+    ID_giver+=1
 
-while bFlag:
+# Main Function
+Flag = True
+time = 0
+while Flag:
+    print("time =", time)
+    for p in personList:
+        """loop 4 peps walking"""
+        p.walk()
+        for z2 in zambieList:
+            if p.get_x() == z2.get_x() and p.get_y() == z2.get_y():
+                sickList.append(Infected(p.get_ID(), p.get_x(), p.get_y()))                    
+                print('PEP',p.get_ID(),"BIT BY ZAMBIE",z2.get_ID(),"!!!")
+                for s2 in sickList:
+                    if p.get_x() == s.get_x() and p.get_y() == s.get_y():
+                        
+                
+            
     for s in sickList:
         """loop 4 peps walking"""
         s.walk()
         
-    print("t = {}, walks complete".format(time))
-    time +=1
-    
-    if time == 100:
-        bFlag = False 
+    for z in zambieList:
+        """loop 4 zams walking"""
+        z.walk()
 
-print("")
-for s in sickList:
-    print(s.__str__(), ", x =", s.get_x(), ", y =", s.get_y())
+    if time == 100:
+        Flag= False
+    time+=1
